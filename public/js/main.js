ced222cdd6a453abc298c59a9ebf7ae90f8d9b81
jdelshad <jdelshad@uci.edu>
2018-05-20 20:23:34 -0700

    url mani for username

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+
+function getURlParameter(urlParam){
+  var pageURl = window.location.search.substring(1);
+  var pageURlVariables = pageURl.split('&');
+  for(var i = 0; i <pageURlVariables.length; i++){
+    var paramaterName = pageURlVariables[i].split('=');
+    if(paramaterName[0] == urlParam){
+      return paramaterName[1];
+    }
+  }
+
+}
+
+
+var username = getURlParameter('username');
+if('undefined' == typeof username || !username){
+  username = 'Anonymous_'+Math.random();
+}
+
+$('#messages').append('<h4>'+username+'</h4>');
+console.log(getURlParameter('username'));
